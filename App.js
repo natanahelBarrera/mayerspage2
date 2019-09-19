e694955744e2d4951b347318026a3875bebb508c
natanahelBarrera <50432209+natanahelBarrera@users.noreply.github.com>
2019-09-18 23:24:04 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className ="form-wrapper">
+        <h1>Congratulations!</h1>
+         <p>Thank you!</p>
+          <p>you successfully submitted your job application.</p>
+          <p>Your application will be review in 1-3 business days.</p>
+      </div>
+      </div>
+    </div>
+  );
+}
+}
+export default App;
